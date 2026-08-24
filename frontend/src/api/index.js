89cf3c2e1571b9bf61ea360a34ef616c7bff9d4c
leo_kf <leonardo.gomez@klearfinance.com>
2026-08-24 14:27:09 +0200

    Added GET cache

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,21 +1,48 @@
+const cacheLifespan = 60 * 60 * 1000;
+
+async function cachedApiCall(call, endpoint, options) {
+  const cacheKey = `api-${endpoint}`;
+  const cached = localStorage.getItem(cacheKey);
+
+  if (cached) {
+    try {
+      const { data, timestamp } = JSON.parse(cached)
+
+      if (Date.now() - timestamp < cacheLifespan) {
+        return data;
+      }
+
+      localStorage.removeItem(cacheKey)
+    } catch {
+      localStorage.removeItem(cacheKey)
+    }
+    return cached;
+  }
+
+  const response = await call(endpoint, options);
+    if (response.status < 300) {
+      localStorage.setItem(
+      cacheKey,
+      JSON.stringify({
+        data: response.data,
+        timestamp: Date.now()
+      })
+    )
+    return response.data;
+  }
+  return 'error';
+}
+
 async function getProducts()  {
-  const response = await window.axios.get(`/product`);
-  if (response.status < 300) {
-    return response.data;
-  }
-  return 'error';
+  const data = await cachedApiCall(window.axios.get, '/product');
+  return data;
 }
-
 async function getProduct(id) {
-  const response = await window.axios.get(`/product/${id}`);
-  if (response.status < 300) {
-    return response.data;
-  }
-  return 'error';
+  const data = await cachedApiCall(window.axios.get, `/product/${id}`);
+  return data;
 }
-async function addToCart()  {
-
-  const response = await window.axios.get(`/cart/${id}`);
+async function addToCart(options)  {
+  const response = await window.axios.post(`/cart`, options);
   if (response.status < 300) {
     return response.data;
   }
